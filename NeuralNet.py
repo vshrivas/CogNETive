f9b522697fec5b4e50294d61dbacd1ce5fa528c9
Vaishnavi Shrivastava <vshrivas@caltech.edu>
2018-12-22 19:21:44 -0800

    implement derivative for squared loss function

--- NeuralNet.py
+++ NeuralNet.py
@@ -26,17 +26,19 @@ class NeuralNet:
     # runs backprop alg on all training examples
     # data_pts: list of numpy arrays, where each array is a single point
     # num_epochs: number of epochs to train this for
-    def train(self, data_pts, num_epochs):
+    def train(self, data_pts, results, num_epochs):
         '''for each example:
             train the neural net for this example
             run backprop alg to recompute the weights'''
         for epoch in num_epochs:
             # randomly sort all data_pts
+            index = 0
             for pt in data_pts:
                 # use the weights and activations to calculate prediction
                 self.feedforward(pt)
                 # improve weights based on cost
-                self.backprop()
+                self.backprop(results[index])
+                index += 1
 
     # generates a prediction (y_hat) based on the input, weights, and activations
     def feedforward(self, data_pt):
@@ -46,10 +48,10 @@ class NeuralNet:
             self.layers[i].layer = self.activation(np.transpose(np.dot(np.transpose(self.layers[i-1]), self.weights[i])))
 
     # uses the backpropogation algorithm to update the weights
-    def backprop(self):
+    def backprop(self, result):
         # calculate last layer error
         # Eo = C'(y_hat) * R'(Zo)
-        self.layer_errors[self.num_layers - 1] = self.loss_der() * self.act_der(self.layer_inputs(self.num_layers - 1))
+        self.layer_errors[self.num_layers - 1] = self.loss_der(result) * self.act_der(self.layer_inputs(self.num_layers - 1))
 
         # calculate layer errors: Eh = E(h+1) * W(h+1) * R'(Z(h)), using backprop + memoization
         # W(h+1) -> [lh, lh+1]
@@ -65,8 +67,16 @@ class NeuralNet:
             self.weights[i] -= np.dot(self.layers[i-1], np.transpose(self.layer_errors[i]))
 
     # evaluates the derivative of the loss function
-    def loss_der(self):
-        pass
+    def loss_der(self, result):
+        if self.loss == "squared":
+            # C = (y_hat - y)^2
+            # C' = 2(y_hat - y)
+            final_layer_size = self.layer_sizes[len(self.layer_sizes) - 1]
+            loss_der_val = np.empty([final_layer_size, 1])
+            for i in range(0, final_layer_size):
+                loss_der_val[i][0] = 2 * (self.layers[self.num_layers - 1].layer[i][0] - result[i][0])
+
+            return loss_der_val
 
     # evaluates the derivative of the activation function for the passed in matrix
     def act_der(self, input):
